99a3e93deebd3286babde691aaf7548bd921cee4
kionawalker <35721387+kionawalker@users.noreply.github.com>
2018-01-26 11:58:16 +0900

    Add files via upload

--- CAE_test.py
+++ CAE_test.py
@@ -10,49 +10,57 @@ from chainer import cuda, Variable, optimizers, Chain, datasets, serializers
 from chainer import training
 from itertools import chain
 import sys
-sys.path.append("~/PycharmProject/Prepare")
-import tools
-sys.path.append("~/PycharmProject/models")
+sys.path.append("/home/common-ns/PycharmProjects/Prepare")
+from tools import load_OULP
+sys.path.append("/home/common-ns/PycharmProjects/models")
 from Convolutional_Auto_Encoder import CAE
 
 
-def plot_mnist_data(samples):
-    for index, (data, label) in enumerate(samples):
-        plt.subplot(2, batch_size, index + 1)
-        plt.axis('off')
-        plt.imshow(data.reshape(128, 88), interpolation='nearest')
+def plot_mnist_data(samples, cnt):
+    if cnt == 0:
+        for index, (data, label) in enumerate(samples):
+            plt.subplot(2, batch_size, index + 1)
+            plt.axis('off')
+            plt.imshow(data.reshape(128, 88), interpolation='nearest', cmap='gray')
 
-        n = int(label)
-        plt.title(n, color='blue')
+            n = int(label)
+            plt.title(n, color='blue')
+            cnt = cnt + 1
 
-    for index, (data, label) in enumerate(samples):
-        plt.subplot(2, batch_size, index + 10)
-        plt.axis('off')
-        plt.imshow(data.reshape(128, 88), interpolation='nearest')
+    if cnt == 1:
+        for index, (data, label) in enumerate(samples):
+            plt.subplot(2, batch_size, index + 11)
+            plt.axis('off')
+            plt.imshow(data.reshape(128, 88), interpolation='nearest', cmap='gray')
 
-        n = int(label)
-        plt.title(n, color='red')
+            n = int(label)
+            plt.title(n, color='red')
+        plt.show()
     list = np.array(samples)
     np.save("/home/common-ns/setoguchi/chainer_files/CAE.npy", list)
 
 
-if __name__=='__main__':
-
-    model = model = L.Classifier(CAE(), lossfun=F.sigmoid_cross_entropy)
-    serializers.load_npz("/home/common-ns/setoguchi/chainer_files/"
-                                 "Convolutional_Auto_Encoder/CAE_model", model)
-    test_dir = "/media/common-ns/New Volume/reseach/Dataset" \
-               "/OU-ISIR_by_Setoguchi/Probe/CV02(Probe)/"
-    test = tools.load_OULP(test_dir)
+if __name__ == '__main__':
+    print "a"
+    model = L.Classifier(CAE(), lossfun=F.sigmoid_cross_entropy)
+    serializers.load_npz("/home/common-ns/setoguchi/chainer_files/Convolutional_Auto_Encoder/result_SCE/model_snapshot_146000", model)
+    test_dir = "/media/common-ns/New Volume/reseach/Dataset/OU-ISIR_by_Setoguchi/Probe/CV02/*"
+    test = load_OULP(test_dir)
+    # print(len(test))
     test = test[0:10]  # 0から10取り出す
     batch_size = 10
+    # print (len(test))
 
+    print "a"
+
+    count = 0
     source = []
     # 原画像の表示
     for (data, label) in test:
         source.append((data, label))
-    plot_mnist_data(source)
-
+    plot_mnist_data(source, count)
+    count = count + 1
+    print "a"
     pred_list = []
     # 復元画像の表示
     for (data, label) in test:
@@ -60,6 +68,7 @@ if __name__=='__main__':
         # pred_data_cpu = cuda.to_cpu(pred_data)
         item = model.__getitem__("loss")
         print(pred_data.shape)
-        print(item)
+        # print(item)
         pred_list.append((pred_data, label))
-    plot_mnist_data(pred_list)
+    plot_mnist_data(pred_list, count)
+
